515ca6c8edc9160ae510d7c08526c2a08c69a000
tcwong2024 <tcwong99@hotmail.com>
2024-11-18 16:19:05 +0800

    Initial Commit

--- files/index.test.js
+++ files/index.test.js
@@ -5,6 +5,6 @@ describe("GET /", () => {
   it('responds with "Hello, Teck Choy!"', async () => {
     const response = await request(app).get("/");
     expect(response.status).toBe(200);
-    expect(response.text).toBe("Hello, world!");
+    expect(response.text).toBe("Hello, Teck Choy!");
   });
 });
